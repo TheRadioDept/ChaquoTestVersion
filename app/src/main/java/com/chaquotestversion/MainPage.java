7f04283c1940721fef550212c2055ae4c2f0a762
Farkhad <farkhadmaratov99@gmail.com>
2022-11-12 17:59:45 +0400

    Changed build.gradle

--- app/src/main/java/com/chaquotestversion/MainPage.java
+++ app/src/main/java/com/chaquotestversion/MainPage.java
@@ -1,6 +1,6 @@
 package com.chaquotestversion;
 
-        import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
         import android.os.Bundle;
         import android.view.View;
--- app/src/main/python/myscript.py
+++ app/src/main/python/myscript.py
@@ -1,6 +1,11 @@
-def main(): 
-	a = 2 
-	b = 2 
-	return a + b
-	
-	
+import numpy as np
+import spacy
+from com.chaquo.python import Python
+def main():
+    #num1= int(number1)
+    #num2= int(number2)
+
+    #summ = num1 + num2
+    #return "sum is "+str(summ)
+    out = np.zeros(5)
+    return "zzeros array "+str(out)
